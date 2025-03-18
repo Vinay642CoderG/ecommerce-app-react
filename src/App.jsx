346a219d5c95d81a22bf9dad71aa22fde0ab20f0
Vinay642CoderG <149595898+Vinay642CoderG@users.noreply.github.com>
2025-03-18 14:44:03 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Home from './screens/Home';
@@ -11,7 +11,7 @@ import CartDetails from './screens/CartDetails';
 const App = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter future={{v7_relativeSplatPath: true, v7_startTransition: true}}>
+      <Router future={{v7_relativeSplatPath: true, v7_startTransition: true}}>
         <Routes>
           <Route path="/" element={<HeaderFooterLayout />}>
             <Route index element={<Home />} />
@@ -21,7 +21,7 @@ const App = () => {
           </Route>
           <Route path="*" element={<h1>404 page not found.</h1>} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 };
